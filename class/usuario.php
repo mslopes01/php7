eba99316db1db60a824b9a96c30044c9e235e6a4
marcelolopes.arc@gmail.com <47823284+mslopes01@users.noreply.github.com>
2019-04-03 15:36:02 -0300

    Alteração da class ususario - delete

--- class/usuario.php
+++ class/usuario.php
@@ -113,6 +113,20 @@
 			));
 		}
 
+		public function delete(){
+
+			$sql = new Sql();
+
+			$sql->query("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+					":ID"=>$this->getUsuario()
+			));
+
+			$this->setUsuario(0);
+			$this->setDeslogin("");
+			$this->setDessenha("");
+			$this->setDtcadastro(new DateTime());
+		}
+
 		public function __construct($login = "", $password =""){
 
 				$this->setDeslogin($login);
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 	require_once("config.php");
-	/*
+/*
 	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = 4");
@@ -11,38 +11,38 @@
 	} else {
 		echo "Resultado não encontrado.";
 	}
-	*/
+*/
 	// Carrega 01 usuario
-	/*
+/*
 	$root = new Usuario();
 
 	$root->loadById(3);
 
 	echo $root;
-	*/
+*/
 
 	// Carrega varios usuarios
-	/*
+/*
 	$lists = Usuario::getList();
 	echo json_encode($lists);
-	*/
+*/
 
 	// Pesquisa por argumento
-	/*
+/*
 	$search = Usuario::search("o");
 	echo json_encode($search);
-	*/
+*/
 
 	// Carrega um usuario  usando login e a senha
-	/*
+/*
 	$usuario = new Usuario();
 	$usuario->login("jose", "1234567890");
 
 	echo $usuario;
-	*/
+*/
 
-	//Criando novo usuario com procidure
-	/*
+	//Criando novo usuario com procedure
+/*
 	$aluno = new Usuario("aluno", "154181");
 
 	//$aluno->setDeslogin("aluno2");
@@ -51,11 +51,17 @@
 	$aluno->insert();
 
 	echo $aluno
-	*/
+*/
 
+	// Alterando usuario
+/*
 	$usuario = new Usuario();
 	$usuario->loadById(8);
 	$usuario->update("professor", "888888");
 	echo $usuario;
-
+*/
+	$usuario = new Usuario();
+	$usuario->loadById(11);
+	$usuario->delete();
+	echo $usuario;
 ?>
