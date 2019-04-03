21733d9e9b947c7eea0a7ff872399ce88d91aaa2
marcelolopes.arc@gmail.com <47823284+mslopes01@users.noreply.github.com>
2019-04-03 11:13:00 -0300

    Alteração da class ususario

--- class/usuario.php
+++ class/usuario.php
@@ -55,6 +55,39 @@
 			}
 		}
 
+		public function getList(){
+
+			$sql = new Sql();
+			return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+		}
+
+		public static function search($login){
+			$sql = new Sql();
+			return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(":SEARCH"=>"%".$login."%"));
+		}
+
+		public function login($login, $password){
+			$sql = new Sql();
+
+			$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+					":LOGIN"=>$login,
+					":PASSWORD"=>$password
+			));
+			if (count($result)) {
+			//if (!empty($result)) {
+				$row = $result[0];
+
+				$this->setUsuario($row['idusuario']);
+				$this->setDeslogin($row['deslogin']);
+				$this->setDessenha($row['dessenha']);
+				$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+			} else {
+				throw new Exception("Login e/ou senha incorreto");
+				
+			}
+		}
+
 		public function __toString(){
 
 			return json_encode(array(
--- index.php
+++ index.php
@@ -1,21 +1,43 @@
 <?php
 
-require_once("config.php");
-/*
-$sql = new Sql();
+	require_once("config.php");
+	/*
+	$sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = 4");
+	$usuarios = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = 4");
 
-if (!empty($usuarios)) {
-	echo json_encode($usuarios);
-} else {
-	echo "Resultado não encontrado.";
-}
-*/
-$root = new Usuario();
+	if (!empty($usuarios)) {
+		echo json_encode($usuarios);
+	} else {
+		echo "Resultado não encontrado.";
+	}
+	*/
+	// Carrega 01 usuario
+	/*
+	$root = new Usuario();
 
-$root->loadById(3);
+	$root->loadById(3);
 
-echo $root;
+	echo $root;
+	*/
+
+	// Carrega varios usuarios
+	/*
+	$lists = Usuario::getList();
+	echo json_encode($lists);
+	*/
+
+	// Pesquisa por argumento
+	/*
+	$search = Usuario::search("o");
+	echo json_encode($search);
+	*/
+
+	// Carrega um usuario  usando login e a senha
+
+	$usuario = new Usuario();
+	$usuario->login("jose", "1234567890");
+
+	echo $usuario;
 
 ?>
